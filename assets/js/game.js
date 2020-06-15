5a24a6fd9aedb4c3062789bdd59441dda13b7377
essennejaye <sjeffers02@gmail.com>
2020-06-14 21:45:36 -0700

    player name bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -134,9 +134,17 @@ var randomNumber = function (min, max) {
 
     return value;
 }
+var getPlayerName = function() {
+    var name ="";
+    while (name === "" || name === null) {
+      name =  prompt("What is your robot's name");
+    }
+    console.log("Your robot's name is " + name)
+    return name;
+}
 // this asks for user input and defines player object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
